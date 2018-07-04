6d7a0cb1410b80441073607e642c4975c3a77706
wnke <silvajp9@gmail.com>
2018-07-04 16:56:04 +0100

    Fix log strings

--- xos/synchronizer/model_policies/policies_hello_world_service.py
+++ xos/synchronizer/model_policies/policies_hello_world_service.py
@@ -4,17 +4,18 @@ from synchronizers.new_base.policy import Policy
 
 log = create_logger(Config().get('logging'))
 
+
 class HelloWordServiceInstancePolicy(Policy):
     model_name = "HelloWorldServiceInstance"
 
     def handle_create(self, model):
         if model.hello_to in ["Tyrion Lannister", "Joffrey Baratheon"]:
-            log.debug("{} is not going to get greeted!".format(model.hello_to), object=str(o), hello_to=o.hello_to)
+            log.debug("{} is not going to get greeted!".format(model.hello_to))
             model.delete()
 
     def handle_update(self, model):
         if model.hello_to in ["Tyrion Lannister", "Joffrey Baratheon"]:
-            log.debug("{} is not going to get greeted!".format(model.hello_to), object=str(o), hello_to=o.hello_to)
+            log.debug("{} is not going to get greeted!".format(model.hello_to))
             model.delete()
 
     def handle_delete(self, model):
