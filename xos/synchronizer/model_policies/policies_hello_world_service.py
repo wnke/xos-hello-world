020e6258d7a65a0d45866785a21d717125cb5d07
wnke <silvajp9@gmail.com>
2018-07-04 16:51:39 +0100

    Add logging and clean up policies file

--- xos/synchronizer/model_policies/policies_hello_world_service.py
+++ xos/synchronizer/model_policies/policies_hello_world_service.py
@@ -1,16 +1,17 @@
-from synchronizers.new_base.modelaccessor import HelloWorldServiceInstance
 from synchronizers.new_base.policy import Policy
 
 
-class MyServiceInstancePolicy(Policy):
+class HelloWordServiceInstancePolicy(Policy):
     model_name = "HelloWorldServiceInstance"
 
     def handle_create(self, model):
         if model.hello_to in ["Tyrion Lannister", "Joffrey Baratheon"]:
+            log.debug("{} is not going to get greeted!".format(model.hello_to), object=str(o), hello_to=o.hello_to)
             model.delete()
 
     def handle_update(self, model):
         if model.hello_to in ["Tyrion Lannister", "Joffrey Baratheon"]:
+            log.debug("{} is not going to get greeted!".format(model.hello_to), object=str(o), hello_to=o.hello_to)
             model.delete()
 
     def handle_delete(self, model):
